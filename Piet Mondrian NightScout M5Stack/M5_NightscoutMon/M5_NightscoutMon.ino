f2655f367823516b153fa90cc741a917e6971a2b
davmartes <50466763+davmartes@users.noreply.github.com>
2019-06-10 10:20:10 +0200

    Update M5_NightscoutMon.ino

--- Piet Mondrian NightScout M5Stack/M5_NightscoutMon/M5_NightscoutMon.ino
+++ Piet Mondrian NightScout M5Stack/M5_NightscoutMon/M5_NightscoutMon.ino
@@ -1,4 +1,26 @@
-// LIVINGROOM CUSTOMIZATION
+/*  M5Stack Nightscout monitor
+    Copyright (C) 2018, 2019 Martin Lukasek <martin@lukasek.cz>
+    CUSTOMIZED VERSION WITH BIGGER TIME AGO, DELTA, CLOCK. NO STARTUP MUSIC. NO BACKGROUND NOISE. SMALLER ARROW. SNOOZE BAR DIFFERENT LOCATION. 
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+    
+    This software uses some 3rd party libraries:
+    IniFile by Steve Marple <stevemarple@googlemail.com> (GNU LGPL v2.1)
+    ArduinoJson by Benoit BLANCHON (MIT License) 
+    IoT Icon Set by Artur Funk (GPL v3)
+
+    PIET MONDRAIN ARTWORK CUSTOMIZATION
+
+*/
 
 
 #include <M5Stack.h>
